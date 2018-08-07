93e66e32256a76837c3ce83d1a5252d276e3d4ec
Michael Hemingway <mpghemingway@gmail.com>
2018-08-07 12:09:44 -0400

    Removed hardcoded nav breadcrumb

--- dist/template-parts/_the_nav.php
+++ dist/template-parts/_the_nav.php
@@ -1,13 +1,14 @@
 <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-	<?php esc_html_e( 'Menu', 'cslpres' ); ?>
-	<span></span><span></span>
+	<i class="screen-reader-text"><?php esc_html_e( 'Menu', 'cslpres' ); ?></i>
+	<span></span>
+	<span></span>
+	<span></span>
 </button>
 
 <header id="masthead" class="site-header">
 	<nav id="global-navigation" class="site-navigation__inner" data-nav="global">
 		<a href="<?= the_permalink(get_page_by_title('ABRA', OBJECT, 'teacherpage' )) ?>" rel="home" title="home">
 			<img class="nav-logo" src="<?= get_template_directory_uri() . '/assets/images/logo_nav.png' ?>">
-			<span class=logo-desc>Abra Teacher Resources - <?= the_title(); ?></span>
 		</a>
 
 		<?php // a lot was removed temporarily. Rethink menu
