00eebb207f367f531025f3de9a9f21784afbea5a
Michael Hemingway <mpghemingway@gmail.com>
2018-08-22 13:12:59 -0400

    Update theme and hide ACF  notice

--- dist/functions/theme-plugins.php
+++ dist/functions/theme-plugins.php
@@ -1,5 +1,11 @@
 <?php
 
+add_filter('site_transient_update_plugins', 'cslpres_remove_update_nag');
+function cslpres_remove_update_nag($value) {
+	unset($value->response[ 'advanced-custom-fields-pro/acf.php' ]);
+	return $value;
+}
+
 add_action('sublanguage_custom_switch', 'cslpres_lang_switch', 10, 2);
 
 /**
