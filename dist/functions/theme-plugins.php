04a54cb33370cfaa6350663188d47231fd15253b
Michael Hemingway <mpghemingway@gmail.com>
2018-08-22 14:11:42 -0400

    Order active plugins first in admin

--- dist/functions/theme-plugins.php
+++ dist/functions/theme-plugins.php
@@ -28,3 +28,30 @@ function cslpres_lang_switch($languages, $sublanguage) {
 <?php
 
 }
+
+/**
+ * Changes plugins display order in the admin, showing active plugins first.
+ * @author Sergey Biryukov http://profiles.wordpress.org/sergeybiryukov/
+ */
+function cslpres_sort_plugins_by_status() {
+	global $wp_list_table, $status;
+	if ( ! in_array( $status, array( 'inactive', 'recently_activated', 'mustuse' ) ) ) {
+		uksort( $wp_list_table->items, '_cslpres_order_callback' );
+	}
+}
+function _cslpres_order_callback( $a, $b ) {
+	global $wp_list_table;
+	$a_active = is_plugin_active( $a );
+	$b_active = is_plugin_active( $b );
+	if ( $a_active && $b_active ) {
+		return strcasecmp( $wp_list_table->items[ $a ]['Name'], $wp_list_table->items[ $b ]['Name'] );
+	} elseif ( $a_active && ! $b_active ) {
+		return -1;
+	} elseif ( ! $a_active && $b_active ) {
+		return 1;
+	} else {
+		return strcasecmp( $wp_list_table->items[ $a ]['Name'], $wp_list_table->items[ $b ]['Name'] );
+	}
+}
+add_action( 'admin_head-plugins.php', 'cslpres_sort_plugins_by_status' );
+
