8c3f7afbdbaad16337d3a3d0d7506ccadef46717
Michael Hemingway <mpghemingway@gmail.com>
2018-08-28 18:04:17 -0400

    initial progress

---
+++ src/modules/activities/src/components/Categories.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="a-meta__container">
+    <div class="meta" v-html="this.thing"></div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import category from './Category'
+
+export default {
+  name: 'Categories',
+  components: { category },
+  created() {
+    this.getthedata()
+  },
+  data() {
+    return {
+      thing: {},
+    }
+  },
+  methods: {
+    getthething() {
+      axios.get('teacherpage/').then(res => {
+        console.log('res: ', res.data)
+      })
+    },
+    getthedata() {
+      axios.get('teacherpage/134').then(res => { // get url from some object set in the php
+        console.log('res.data: ', res.data)
+        this.thing = res.data.content.rendered
+      })
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+  .a-meta__container {
+    padding: 2rem;
+    margin-top: 5rem;
+  }
+</style>
---
+++ src/modules/activities/src/components/Category.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1></h1>
+</template>
+
+<script>
+export default {
+  name: 'Category',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
